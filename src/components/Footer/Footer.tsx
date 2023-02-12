71ea2180d8e4cabda577a1146cee753fa7515414
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2023-02-12 20:43:05 +0200

    fix footer padding on xs

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { typography } from "@/essentials/theme/typography";
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center justify-between px-6 sm:px-8 pb-6 sm:pb-8 max-w-screen-lg m-auto">
+    <footer className="flex items-center justify-between px-4 sm:px-8 pb-6 sm:pb-8 max-w-screen-lg m-auto">
       <div>
         <span
           className={`${typography.variants.caption} block`}
