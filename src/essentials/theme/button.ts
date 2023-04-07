a0975f588d3c5bc60453eb2e2f87568123432b2c
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2023-04-07 16:55:12 +0300

    fix inverted button font color
    caused by tailwind class declaration order

--- src/essentials/theme/button.ts
+++ src/essentials/theme/button.ts
@@ -1,12 +1,13 @@
 const buttonBase =
-  "flex justify-center items-center inline-block border-gray-800 font-primary text-gray-800 font-semibold hover:bg-gray-800 hover:text-white";
-const inverted = `${buttonBase} bg-gray-800 text-white hover:bg-white hover:text-gray-800`;
+  "flex justify-center items-center inline-block border-gray-800 font-semibold ";
+const buttonPrimary = `${buttonBase} font-primary text-gray-800 hover:bg-gray-800 hover:text-white`;
+const buttonInverted = `${buttonBase} bg-gray-800 text-white hover:bg-white hover:text-gray-800`;
 
 const variants = {
-  large: `${buttonBase} p-4 border-3 text-xl`,
-  smol: `${buttonBase} p-2 border-2`,
-  largeInverted: `${inverted} p-4 border-3 text-xl`,
-  smolInverted: `${inverted} p-2 border-2`,
+  large: `${buttonPrimary} p-4 border-3 text-xl`,
+  smol: `${buttonPrimary} p-2 border-2`,
+  largeInverted: `${buttonInverted} p-4 border-3 text-xl`,
+  smolInverted: `${buttonInverted} p-2 border-2`,
 };
 
 export const button = { variants };
