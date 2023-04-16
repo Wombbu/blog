f84a74a8f27e25651d778e077b909cba0f4da07f
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2023-04-16 19:35:26 +0300

    add clap button, re-make social media share section

---
+++ src/app/model/claps/getClaps.ts
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+type Response = {
+  claps: number;
+};
+
+const urlBuilder = (slug: string) =>
+  `http://localhost:3000/api/get-claps?slug=${slug}`;
+
+export const fetchClaps = async (slug: string): Promise<Response> => {
+  const response = await fetch(urlBuilder(slug));
+  return response.json();
+};
