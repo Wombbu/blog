695dc960f85f6c846111688e576b33ac8ec8b427
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2023-10-01 01:39:39 +0300

    fix error

--- src/pages/api/update-claps.tsx
+++ src/pages/api/update-claps.tsx
@@ -11,71 +11,71 @@ export default async function updateClaps(
   res.status(200).json({});
   return;
 
-  try {
-    const q = faunadb.query;
+  // try {
+  //   const q = faunadb.query;
 
-    const client = new faunadb.Client({
-      secret: process.env.FAUNA_SECRET_KEY || "",
-    });
+  //   const client = new faunadb.Client({
+  //     secret: process.env.FAUNA_SECRET_KEY || "",
+  //   });
 
-    const { slug, count } = req.query;
+  //   const { slug, count } = req.query;
 
-    const incrementClapsCount = Number(count);
+  //   const incrementClapsCount = Number(count);
 
-    if (!slug) {
-      res.status(400).json({
-        message:
-          "Article slug not provided. Please provide 'slug' and 'count' query parameters",
-      });
+  //   if (!slug) {
+  //     res.status(400).json({
+  //       message:
+  //         "Article slug not provided. Please provide 'slug' and 'count' query parameters",
+  //     });
 
-      return;
-    }
+  //     return;
+  //   }
 
-    if (
-      isNaN(incrementClapsCount) ||
-      incrementClapsCount > 50 ||
-      incrementClapsCount < 0
-    ) {
-      res.status(400).json({
-        message:
-          "Clap count not provided or is of wrong format or is too large or negative. Please provide 'slug' and 'count' query parameters",
-      });
-      return;
-    }
+  //   if (
+  //     isNaN(incrementClapsCount) ||
+  //     incrementClapsCount > 50 ||
+  //     incrementClapsCount < 0
+  //   ) {
+  //     res.status(400).json({
+  //       message:
+  //         "Clap count not provided or is of wrong format or is too large or negative. Please provide 'slug' and 'count' query parameters",
+  //     });
+  //     return;
+  //   }
 
-    // Check and see if the doc exists.
-    const doesDocExist = await client.query(
-      q.Exists(q.Match(q.Index("claps_by_slug"), slug))
-    );
+  //   // Check and see if the doc exists.
+  //   const doesDocExist = await client.query(
+  //     q.Exists(q.Match(q.Index("claps_by_slug"), slug))
+  //   );
 
-    if (!doesDocExist) {
-      await client.query(
-        q.Create(q.Collection("claps"), {
-          data: { slug: slug, claps: 0 },
-        })
-      );
-    }
-    // Fetch the document for-real
-    const document: {
-      ref: faunadb.ExprArg;
-      data: {
-        claps: number;
-      };
-    } = await client.query(q.Get(q.Match(q.Index("claps_by_slug"), slug)));
+  //   if (!doesDocExist) {
+  //     await client.query(
+  //       q.Create(q.Collection("claps"), {
+  //         data: { slug: slug, claps: 0 },
+  //       })
+  //     );
+  //   }
+  //   // Fetch the document for-real
+  //   const document: {
+  //     ref: faunadb.ExprArg;
+  //     data: {
+  //       claps: number;
+  //     };
+  //   } = await client.query(q.Get(q.Match(q.Index("claps_by_slug"), slug)));
 
-    await client.query(
-      q.Update(document.ref, {
-        data: {
-          claps: document.data.claps + incrementClapsCount,
-        },
-      })
-    );
+  //   await client.query(
+  //     q.Update(document.ref, {
+  //       data: {
+  //         claps: document.data.claps + incrementClapsCount,
+  //       },
+  //     })
+  //   );
 
-    res.status(200).json({});
-    return;
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Internal Server Error" });
-    return;
-  }
+  //   res.status(200).json({});
+  //   return;
+  // } catch (e) {
+  //   console.log(e);
+  //   res.status(500).json({ message: "Internal Server Error" });
+  //   return;
+  // }
 }
