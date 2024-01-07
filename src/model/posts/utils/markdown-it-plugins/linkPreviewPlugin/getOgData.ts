9e91f3c16bfa44195510db2403d28f8931f1e5b7
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2024-01-07 14:50:18 +0200

    replace link preview plugin jsdom with node-html-parser to drastically reduce sub dependency count

--- src/model/posts/utils/markdown-it-plugins/linkPreviewPlugin/getOgData.ts
+++ src/model/posts/utils/markdown-it-plugins/linkPreviewPlugin/getOgData.ts
@@ -1,5 +1,5 @@
 import { OgData } from "@/model/posts/utils/markdown-it-plugins/linkPreviewPlugin/OgData";
-import { JSDOM } from "jsdom";
+import { parse } from "node-html-parser";
 import { getHtmlSync } from "./getHtmlSync";
 
 export function getOgData(url: string) {
@@ -13,53 +13,49 @@ export function getOgData(url: string) {
 
   try {
     const html = getHtmlSync(url);
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
+    const doc = parse(html);
 
     // title
     const titleSelector = doc.querySelector("title");
     const title = titleSelector != null ? titleSelector.textContent : undefined;
-    const titleMetaSelector = doc.querySelector('meta[property="og:title"]');
-    const titleMeta =
-      // @ts-expect-error
-      titleMetaSelector != null ? titleMetaSelector.content : undefined;
+    const titleMeta = doc
+      .querySelector('meta[property="og:title"]')
+      ?.getAttribute("content");
 
-    ogData.title = titleMeta ? titleMeta : title;
+    ogData.title = titleMeta || title || "";
 
     // description
-    const descSelector = doc.querySelector('meta[name="description"]');
-    // @ts-expect-error
-    let desc = descSelector != null ? descSelector.content : undefined;
-    const desc2Selector = doc.querySelector('meta[name="Description"]');
-    // @ts-expect-error
-    const desc2 = desc2Selector != null ? desc2Selector.content : undefined;
-    desc = desc ? desc : desc2;
-    const descMetaSelector = doc.querySelector(
-      'meta[property="og:description"]'
-    );
-    const descMeta =
-      // @ts-expect-error
-      descMetaSelector != null ? descMetaSelector.content : undefined;
-    ogData.description = descMeta ? descMeta : desc;
+    const desc = doc
+      .querySelector('meta[name="description"]')
+      ?.getAttribute("content");
+    const desc2 = doc
+      .querySelector('meta[name="Description"]')
+      ?.getAttribute("content");
+    const descMeta = doc
+      .querySelector('meta[property="og:description"]')
+      ?.getAttribute("content");
+
+    ogData.description = descMeta || desc || desc2 || "";
 
     // image
-    const imageMetaSelector = doc.querySelector('meta[property="og:image"]');
-    ogData.image =
-      // @ts-expect-error
-      imageMetaSelector != null ? imageMetaSelector.content : undefined;
+    const imageMeta = doc
+      .querySelector('meta[property="og:image"]')
+      ?.getAttribute("content");
+    ogData.image = imageMeta || "";
 
     // site_name
-    const siteMetaSelector = doc.querySelector('meta[property="og:site_name"]');
-    ogData.site_name =
-      // @ts-expect-error
-      siteMetaSelector != null ? siteMetaSelector.content : undefined;
+    const siteMeta = doc
+      .querySelector('meta[property="og:site_name"]')
+      ?.getAttribute("content");
+    ogData.site_name = siteMeta || "";
 
     // url
-    const urlMetaSelector = doc.querySelector('meta[property="og:url"]');
-    const urlMeta =
-      // @ts-expect-error
-      urlMetaSelector != null ? urlMetaSelector.content : undefined;
-    ogData.url = urlMeta ? urlMeta : url;
+    const urlMeta = doc
+      .querySelector('meta[property="og:url"]')
+      ?.getAttribute("content");
+    ogData.url = urlMeta || url;
+
+    return ogData;
   } catch (error) {
     console.log(error);
   }
