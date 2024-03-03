a7ee45ec05a3db2e2466f059f80d2e32b6f00a68
Lauri Nevanperä <lauri.nevanpera@almamedia.fi>
2024-03-03 20:04:47 +0200

    changes to cta section, fix hero img caption padding

--- src/app/(default-layout)/posts/[slug]/components/CtaSection.tsx
+++ src/app/(default-layout)/posts/[slug]/components/CtaSection.tsx
@@ -4,6 +4,8 @@ import { SocialMediaLinks } from "@/components/SocialMediaLinks";
 import dynamic from "next/dynamic";
 import { palette } from "@/essentials/theme/palette";
 import { Post } from "@/model/posts/types/Post";
+import { button } from "@/essentials/theme/button";
+import Image from "next/image";
 
 // https://beta.nextjs.org/docs/rendering/static-and-dynamic-rendering
 const LazySocialMediaSharePost = dynamic(
@@ -24,37 +26,43 @@ type Props = {
 export const CtaSection = ({ post }: Props) => {
   return (
     <FullWidthGray>
-      <div className="flex flex-1 flex-col items-stretch gap-6 max-w-article m-auto">
+      <div className="flex flex-1 flex-col items-center gap-4 max-w-article m-auto">
         <div>
           <h1
             className={`${typography.variants.sectionTitle({
               noGutter: true,
-            })} mb-1`}
+            })}`}
           >
             Kiitos kun luit.
           </h1>
-          <h2
-            className={`${typography.variants.textBody} ${palette.text.secondary}`}
-          >
-            Jaa jos pidit. Pidä aihe keskustelussa.
-          </h2>
         </div>
         <LazySocialMediaSharePost post={post} />
-        <div>
-          <h1
-            className={`${typography.variants.sectionTitle({
-              noGutter: true,
-            })}  mb-1`}
-          >
-            Kiinnostuitko?
-          </h1>
-          <h2
-            className={`${typography.variants.textBody} ${palette.text.secondary}`}
-          >
-            Kerron uusista artikkeleista Twitterissä, Threadsissa ja
-            Instagramissa.
-          </h2>
-        </div>
+        <h1
+          className={`${typography.variants.sectionTitle({
+            noGutter: true,
+          })} text-center mt-6`}
+        >
+          🔔 Tilaa artikkelit puhelimeesi
+        </h1>
+        <a
+          href="https://chat.whatsapp.com/EiQjzdPh1qq6AOt7nnj2vG"
+          className={`${button.variants.rounded}`}
+        >
+          <Image
+            src="/whatsapp.svg"
+            alt="whatsapp-logo"
+            width={22}
+            height={22}
+            className={`mr-2`}
+          />{" "}
+          Tilaa ilmoitukset
+        </a>
+        <h2
+          className={`${typography.variants.textBody} ${palette.text.secondary} text-center`}
+        >
+          Kerron uusista artikkeleista myös Twitterissä, Threadsissa ja
+          Instagramissa.
+        </h2>
         <SocialMediaLinks />
       </div>
     </FullWidthGray>
